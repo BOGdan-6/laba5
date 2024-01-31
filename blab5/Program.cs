b45a42842ff3c015afa77e43b127d5ba428e1c2d
danil <dddd@gmail.com>
2024-01-31 16:32:24 +0300

    Добавьте файлы проекта.

---
+++ blab5/Program.cs
@@ -0,0 +1,16 @@
+﻿
+    const double E = 0.001;
+    Console.Write("Введите значение x=");
+    double x = double.Parse(Console.ReadLine());
+    double S = 0;
+    int n = 0;
+    double a = x / 3;
+    do
+    {
+        S = S + a;
+        n++;
+        a *= (n + 1) / (Math.Pow(3, n / 2.0) * n);
+    }
+    while (a >= E);
+    Console.WriteLine($"Сумма S= {S}");
+Console.ReadLine();
